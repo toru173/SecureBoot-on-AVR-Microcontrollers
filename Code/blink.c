27d1ca2b749f6fe32c601695638f806ebb2d973b
toru173 <toru173+github@gmail.com>
2021-04-02 16:30:20 +1000

    Update blink.c

--- Code/blink.c
+++ Code/blink.c
@@ -1,16 +1,34 @@
-#define F_CPU 1000000UL
+#define F_CPU 8000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-int
-main (void)
+/*
+ * This demonstrate how to use the avr_mcu_section.h file
+ * The macro adds a section to the ELF file with useful
+ * information for the simulator
+ */
+#include "avr_mcu_section.h"
+AVR_MCU(F_CPU, "atmega2580");
+
+/*
+ * This small section tells simavr to generate a VCD trace dump with changes to these
+ * registers.
+ */
+const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
+    { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, },
+};
+
+int main (void)
 {
     DDRB |= _BV(DDB0);
     
-    while(1)
+    int count = 0;
+    
+    while(count < 10)
     {
         PORTB ^= _BV(PB0);
         _delay_ms(500);
+        count++;
     }
 }
