12e71a1a9576131ca9fc2028316f85d8bf966e50
toru173 <toru173+github@gmail.com>
2021-04-03 15:16:56 +1000

    Update hello_world_uart.c

--- Code/hello_world_uart.c
+++ Code/hello_world_uart.c
@@ -56,10 +56,13 @@ int main (void)
     while(i < 12)
     {
         loop_until_bit_is_set(UCSR0A, UDRE0); // Wait for transmit buffer to be empty
-        UDR0 = i + 48;
+        UDR0 = i + 65;
         i++;
     }
      
+    loop_until_bit_is_set(UCSR0A, UDRE0); // Wait for transmit buffer to be empty
+    UDR0 = 0x0A;
+    
     _delay_ms(500);
     
     // this quits the simulator, since interupts are off
