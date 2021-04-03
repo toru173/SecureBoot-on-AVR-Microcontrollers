2f1268011100c7067bbed3db3632d479de4419c7
toru173 <toru173+github@gmail.com>
2021-04-03 18:22:43 +1000

    Update hello_world_uart.c

--- Code/hello_world_uart.c
+++ Code/hello_world_uart.c
@@ -65,7 +65,7 @@ int main (void)
     uart_init();
     // stdin = stdout = stderr = &uart_stdio;
 
-    unsigned char message[] = "Hello World!";
+    static unsigned char message[] = "Hello World!";
     
     // fprintf(&uart_stdio, "H");
     // int i = 0;
@@ -96,7 +96,7 @@ int main (void)
     
     for (int i = 0; i < 12; i ++)
     {
-        uart_putchar(*message[i]);
+        uart_putchar(message[i]);
     }
     
     uart_putchar('\n');
