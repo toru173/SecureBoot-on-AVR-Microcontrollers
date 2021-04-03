45832dd44495cbf19e9e8478e0389ef9f0559492
toru173 <toru173+github@gmail.com>
2021-04-03 14:30:20 +1000

    Update hello_world_uart.c
    Changed to using standard avr-gcc library

--- Code/hello_world_uart.c
+++ Code/hello_world_uart.c
@@ -5,13 +5,13 @@
  */
 
 #define F_CPU 8000000UL // 8MHz oscillator
-#define BAUD 9600 // Could be faster if desired. This is just a PoC
 
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
-#include <util/setbaud.h>
-#include "uart.h"
+#include <uart.h>
+
+
 
 /*
  * This demonstrate how to use the avr_mcu_section.h file
@@ -39,26 +39,11 @@ const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
 
 unsigned char message[] = "Hello World!";
 
-void init_uart_0(void)
-{
-    UCSR0A = 0x00; // Clear USART0 status register
-    UBRR0H = UBRRH_VALUE; /* Set baud values correctly
-    UBRR0L = UBRRL_VALUE;  */
-    UCSR0B = (1 << RXEN0 ) | (1 << TXEN0); // Enable transmit & receive
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8 bit, no parity, one stop bit
-    DDRE &= ~(1 << PE0); // Enable input on Port E (RXD0)
-    DDRE |= (1 << PE1); // Enable output on Port E (TXD0)
-}
-
 int main (void)
 {
-    init_uart_0();
+    uart_init(9600);
     
-    for (int i = 0; i < 11; i++)
-    {
-        loop_until_bit_is_set(UCSR0A, UDRE0); // Wait for transmit buffer to be empty
-        UDR0 = message[i];
-    }
+    uart_puts(message);
     
     // this quits the simulator, since interupts are off
     // this is a "feature" that allows running tests cases and exit
--- Code/uart.h
+++
@@ -1,5 +0,0 @@
-/*
- * Header files for uart comms
- */
-
-void init_uart_0(void);
