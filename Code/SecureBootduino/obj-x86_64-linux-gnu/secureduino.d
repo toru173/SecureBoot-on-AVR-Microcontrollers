79c87fa8c9bdcc27eb011c614ea257c88e2e7067
toru173 <toru173+github@gmail.com>
2021-04-05 22:42:10 +1000

    hex file and object exports

---
+++ Code/SecureBootduino/obj-x86_64-linux-gnu/secureduino.d
@@ -0,0 +1,11 @@
+obj-x86_64-linux-gnu/secureduino.o: secureduino.c \
+ simavr/simavr/sim/sim_avr.h simavr/simavr/sim/sim_irq.h \
+ simavr/simavr/sim/sim_interrupts.h simavr/simavr/sim/sim_avr_types.h \
+ simavr/simavr/sim/fifo_declare.h simavr/simavr/sim/sim_cmds.h \
+ simavr/simavr/sim/sim_cycle_timers.h simavr/simavr/sim/sim_io.h \
+ simavr/simavr/sim/sim_avr.h simavr/simavr/sim/sim_regbit.h \
+ simavr/simavr/sim/avr_ioport.h simavr/simavr/sim/sim_elf.h \
+ simavr/simavr/sim/avr/avr_mcu_section.h simavr/simavr/sim/sim_hex.h \
+ simavr/simavr/sim/sim_gdb.h simavr/examples/parts/uart_pty.h \
+ simavr/simavr/sim/sim_irq.h simavr/simavr/sim/fifo_declare.h \
+ simavr/simavr/sim/sim_vcd_file.h
---
+++ Code/SecureBootduino/obj-x86_64-linux-gnu/uart_pty.d
@@ -0,0 +1,9 @@
+obj-x86_64-linux-gnu/uart_pty.o: simavr/examples/parts/uart_pty.c \
+ simavr/simavr/sim/sim_network.h simavr/examples/parts/uart_pty.h \
+ simavr/simavr/sim/sim_irq.h simavr/simavr/sim/fifo_declare.h \
+ simavr/simavr/sim/avr_uart.h simavr/simavr/sim/sim_avr.h \
+ simavr/simavr/sim/sim_irq.h simavr/simavr/sim/sim_interrupts.h \
+ simavr/simavr/sim/sim_avr_types.h simavr/simavr/sim/fifo_declare.h \
+ simavr/simavr/sim/sim_cmds.h simavr/simavr/sim/sim_cycle_timers.h \
+ simavr/simavr/sim/sim_io.h simavr/simavr/sim/sim_regbit.h \
+ simavr/simavr/sim/sim_time.h simavr/simavr/sim/sim_hex.h
