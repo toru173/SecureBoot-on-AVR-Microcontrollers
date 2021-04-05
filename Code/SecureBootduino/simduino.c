1751a92f79527227736152aab50cbbdc30080dcc
toru173 <toru173+github@gmail.com>
2021-04-05 17:58:59 +1000

    Revert "Force Atmega2560"
    This reverts commit cd37638c360bfca0cdd0c99b98d55b9fb8e70e51.

--- Code/SecureBootduino/simduino.c
+++ Code/SecureBootduino/simduino.c
@@ -92,8 +92,8 @@ int main(int argc, char *argv[])
 	struct avr_flash flash_data;
 	char boot_path[1024] = "securebootloader.hex";
 	uint32_t boot_base, boot_size;
-	char * mmcu = "atmega2560";
-	uint32_t freq = 20000000;
+	char * mmcu = "atmega328p";
+	uint32_t freq = 16000000;
 	int debug = 0;
 	int verbose = 0;
 
