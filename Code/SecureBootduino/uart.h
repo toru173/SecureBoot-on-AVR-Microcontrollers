ce5ce3e3733997d0968b1038d87b75a9176b3752
toru173 <toru173+github@gmail.com>
2021-04-07 15:08:46 +1000

    Update uart.h

--- Code/SecureBootduino/uart.h
+++ Code/SecureBootduino/uart.h
@@ -6,11 +6,21 @@
 #define USERICHSTRINGS
 #endif
 
-#include <stdio.h>
-
 void uart_init(void);
+
+#ifdef USERICHSTRINGS
+#include <stdio.h>
 int uart_putchar(char c, FILE *stream);
 int uart_getchar(FILE *stream);
+#endif
+
+int uart_putrawchar(char c)
+int uart_getrawchar()
+void my_printf(char *string)
+
+char *bytetohex(uint8_t byte)
+uint8_t hextobyte(char hex[2])
+
 
 void uart_init(void)
 {
