27e8c4fa2492f0b0e532de25dab04d78b4697f00
toru173 <toru173+github@gmail.com>
2021-04-07 14:38:20 +1000

    Update uart.h

--- Code/SecureBootduino/uart.h
+++ Code/SecureBootduino/uart.h
@@ -65,17 +65,17 @@ void my_printf(char *string)
 char *bytetohex(uint8_t byte)
 {
     char *c = "\0\0";
-    uint8_t nybble0 = (byte >> 4) & 0x0F
-    uint8_t nybble1 = byte& 0x0F
+    uint8_t nybble0 = (byte >> 4) & 0x0F;
+    uint8_t nybble1 = byte& 0x0F;
     if (nybble0 > 0x09)
-        nybble0 += 0x41;
+        nybble0 += 0x41; // A-F
     else
-        nybble0 += 0x30;
+        nybble0 += 0x30; // 0-9
 
     if (nybble1 > 0x09)
-        nybble1 += 0x41;
+        nybble1 += 0x41; // A-F
     else
-        nybble1 += 0x30;
+        nybble1 += 0x30; // 0-9
     c[0] = nybble0;
     c[1] = nybble1;
     return c
