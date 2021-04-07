701136f9d8175c217e1e4b6876603f7fbbe47509
toru173 <toru173+github@gmail.com>
2021-04-07 16:07:32 +1000

    Update uart.h

--- Code/SecureBootduino/uart.h
+++ Code/SecureBootduino/uart.h
@@ -18,10 +18,6 @@ int uart_putrawchar(char c);
 int uart_getrawchar();
 void my_printf(char *string);
 
-char *bytetohex(uint8_t byte);
-uint8_t hextobyte(char hex[2]);
-
-
 void uart_init(void)
 {
     UCSR0A = 0x00; // Clear USART0 status register
@@ -80,30 +76,4 @@ void my_printf(char *string)
     {
         uart_putrawchar(string[i]);
     }
-}
-
-char *bytetohex(uint8_t byte)
-{
-    char *c = "\0\0";
-    uint8_t nybble0 = (byte >> 4) & 0x0F;
-    uint8_t nybble1 = byte& 0x0F;
-    if (nybble0 > 0x09)
-        nybble0 += 0x37; // A-F
-    else
-        nybble0 += 0x30; // 0-9
-
-    if (nybble1 > 0x09)
-        nybble1 += 0x37; // A-F
-    else
-        nybble1 += 0x30; // 0-9
-    c[0] = nybble0;
-    c[1] = nybble1;
-    return c;
-}
-
-// Finish me later
-uint8_t hextobyte(char hex[2])
-{
-    uint8_t byte = 0x00;
-    return byte;
 }
