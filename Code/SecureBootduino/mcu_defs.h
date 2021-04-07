98e4bf66a6faeeb379f6ef577426448d1947f00e
toru173 <toru173+github@gmail.com>
2021-04-07 19:08:07 +1000

    Update mcu_defs.h

--- Code/SecureBootduino/mcu_defs.h
+++ Code/SecureBootduino/mcu_defs.h
@@ -1,8 +1,8 @@
 /*
- * Header files for mcu comms
+ * Header files for mcu definitions. Used in secure_bootloader, secure_payload and secureduino
  */
 
 #define MCU "atmega328p"
 #define CLOCK 16000000
-#define BLS_START 0x7000
+#define BLS_START 0x7000 // Equivalent to BOOTSZ1 = 0, BOOTSZ0 = 0. 4K of bootloader space
 #define UART_BAUD 57600
