ef61350e83390e73be1eeeb89e087570979de235
toru173 <toru173+github@gmail.com>
2021-04-07 12:52:23 +1000

    Update uart_pty.c

--- Code/SecureBootduino/simavr/examples/parts/uart_pty.c
+++ Code/SecureBootduino/simavr/examples/parts/uart_pty.c
@@ -324,6 +324,5 @@ uart_pty_connect(
 				p->tap.slavename);
 		system(cmd);
 	} else
-		printf("note: Use picocom to get a terminal on the above tty\n\
-                Execution paused until a character is pressed\n");
+		printf("note: Use picocom to get a terminal on the above tty\nExecution paused until a key is pressed\n");
 }
