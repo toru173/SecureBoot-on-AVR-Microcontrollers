45bdaf96d02d1bd82df00f783ab437c56b56a15c
toru173 <toru173+github@gmail.com>
2021-04-07 17:25:38 +1000

    Create monitor.c

---
+++ Code/SecureBootduino/include/monitor.c
@@ -0,0 +1,31 @@
+/*
+ * Header files for monitor comms
+ */
+
+#include "monitor.h"
+
+char *bytetohex(uint8_t byte)
+{
+    char *c = "\0\0";
+    uint8_t nybble0 = (byte >> 4) & 0x0F;
+    uint8_t nybble1 = byte& 0x0F;
+    if (nybble0 > 0x09)
+        nybble0 += 0x37; // A-F
+    else
+        nybble0 += 0x30; // 0-9
+
+    if (nybble1 > 0x09)
+        nybble1 += 0x37; // A-F
+    else
+        nybble1 += 0x30; // 0-9
+    c[0] = nybble0;
+    c[1] = nybble1;
+    return c;
+}
+
+// Finish me later
+uint8_t hextobyte(char hex[2])
+{
+    uint8_t byte = 0x00;
+    return byte;
+}
