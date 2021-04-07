6b4dfb4c0b527c96978d8cd4ba1d07aa0ba592a0
toru173 <toru173+github@gmail.com>
2021-04-07 13:35:39 +1000

    Update secure_bootloader.c

--- Code/SecureBootduino/secure_bootloader.c
+++ Code/SecureBootduino/secure_bootloader.c
@@ -39,10 +39,15 @@ int main (void)
     uart_init();
     stdout = stdin = stderr = &uart_stdio;
     
+    u16int_t flashpointer = 0x0000;
+    
     // Wait until we connect to the UART
     uart_getrawchar();
     
+    u8int_t byte = pgm_read_byte(flashpointer);
+    
     printf("\nHere we go!\n");
+    printf(byte + 0x30); //convert to ASCII
     
     run_firmware();
 }
