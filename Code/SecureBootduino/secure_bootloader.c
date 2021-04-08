3f12480b2cb1f501f70aa08312ad5fb4c0fc80e7
toru173 <toru173+github@gmail.com>
2021-04-08 13:52:32 +1000

    Update secure_bootloader.c

--- Code/SecureBootduino/secure_bootloader.c
+++ Code/SecureBootduino/secure_bootloader.c
@@ -77,9 +77,10 @@ int main (void)
         // Enter monitor
         uint16_t *blockptr = get512block(0x7000);
         my_printf("\ngot block!\n");
+        
         for (int i = 0; i < 64; i++)
         {
-            my_printf(bytetohex((uint8_t) *(blockptr + i)));
+            my_printf(bytetohex(*(blockptr + i)));
         }
         free(blockptr);
     }
