8d25b26cc2faa34449b0e024e4a1fd2712d100dc
toru173 <toru173+github@gmail.com>
2021-04-08 12:42:54 +1000

    Update secure_bootloader.c

--- Code/SecureBootduino/secure_bootloader.c
+++ Code/SecureBootduino/secure_bootloader.c
@@ -52,7 +52,7 @@ uint16_t *get512block(uint16_t *baseaddress) // Address always less than 64K
 {
     uint16_t *buffer = malloc(64); // 64 byte (512 bit) buffer
     if (!buffer) // Unable to allocate
-        return -1;
+        return NULL;
     for (uint16_t i = *baseaddress; i < *(baseaddress + 64); i++)
         *(buffer + i) = pgm_read_byte(baseaddress + i);
     return buffer;
@@ -72,8 +72,9 @@ int main (void)
     {
         // Enter monitor
         uint16_t blockptr = *get512block(0x0000);
-        my_printf(blockptr);
-        free(*blockptr);
+        for (int i = 0; i < 64; i++)
+            my_printf(char(blockptr + i))
+        free(blockptr);
     }
         
     run_firmware();
