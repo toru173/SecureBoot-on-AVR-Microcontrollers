81858c0c7fd75481aa2c05ba809446772c2817c5
toru173 <toru173+github@gmail.com>
2021-04-08 16:27:38 +1000

    Update secure_bootloader.c

--- Code/SecureBootduino/secure_bootloader.c
+++ Code/SecureBootduino/secure_bootloader.c
@@ -93,7 +93,7 @@ int main (void)
         for (i = 0; i < 64; i++)
           h[i] = avrnacl_sha512_iv[i];
         
-        uint16_t *blockptr = get1024block(0x0000);
+        uint16_t blockptr = get1024block(0x0000);
         
         for (int i = 0; i < 128; i++)
             m[i] = blockptr[i];
