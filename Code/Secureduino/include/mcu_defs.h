ce924ac277891e237508cc1f42ecd1cf9d3491df
toru173 <toru173+github@gmail.com>
2021-04-09 19:56:00 +1000

    Update mcu_defs.h

--- Code/Secureduino/include/mcu_defs.h
+++ Code/Secureduino/include/mcu_defs.h
@@ -2,7 +2,17 @@
  * Header files for mcu definitions. Used in secure_bootloader, secure_payload and secureduino
  */
 
+// atmega328p or atmega2560
 #define MCU "atmega328p"
 #define CLOCK 16000000
-#define BLS_START 0x7000 // Equivalent to BOOTSZ1 = 0, BOOTSZ0 = 0. 4K of bootloader space (2K words)
+
+// Define bootloader address. 4K on atmega328p = 0x7000, 8K on atmega2560 = 0x3FE00
+#if MCU = "atmega328p"
+    #define BLS_START 0x7000
+#elif MCU = "atmega2560"
+    #define BLS_START 0x3FE00
+#else
+    #error "Unexpected MCU"
+#endif
+
 #define UART_BAUD 9600
