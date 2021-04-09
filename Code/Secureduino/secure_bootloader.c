f00f555522dc9de9367384534f78d2eb2413518a
toru173 <toru173+github@gmail.com>
2021-04-09 21:14:22 +1000

    Update secure_bootloader.c

--- Code/Secureduino/secure_bootloader.c
+++ Code/Secureduino/secure_bootloader.c
@@ -64,9 +64,8 @@ int main (void)
         
     char c = uart_getrawchar(); // Wait for input before continuing
     
-    c = uart_getrawchar(); // Wait for input before continuing
-    c = uart_getrawchar(); // Wait for input before continuing
-
+    while (1)
+        raw_printf("Hello from the bootloader!\n");
     
     if (c == 'm')
     {
