9b50f8d118efdab5d93d7277c96ebe8b7c39d383
toru173 <toru173+github@gmail.com>
2021-04-09 20:33:02 +1000

    Update mcu_defs.h

--- Code/Secureduino/lib/mcu_defs.h
+++ Code/Secureduino/lib/mcu_defs.h
@@ -2,6 +2,7 @@
  * Header files for mcu definitions. Used in secure_bootloader, secure_payload and secureduino
  */
 
+<<<<<<< HEAD:Code/Secureduino/lib/mcu_defs.h
 // valid MCUs include atmega328p or atmega2560
 #define ATMEGA328P "atmega328p"
 #define ATMEGA2560 "atmega2560"
@@ -18,4 +19,9 @@
     #error "Unexpected MCU"
 #endif
 
+=======
+#define MCU "atmega328p"
+#define CLOCK 16000000
+#define BLS_START 0x7000 // Equivalent to BOOTSZ1 = 0, BOOTSZ0 = 0. 4K of bootloader space (2K words)
+>>>>>>> parent of ce924ac (Update mcu_defs.h):Code/Secureduino/include/mcu_defs.h
 #define UART_BAUD 9600
