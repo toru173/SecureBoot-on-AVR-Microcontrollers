f7fc6e82369f0cc5d69e97505fb84846e29d0cf1
toru173 <toru173+github@gmail.com>
2021-04-09 21:15:57 +1000

    Update secureduino.c

--- Code/Secureduino/secureduino.c
+++ Code/Secureduino/secureduino.c
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
 {
 	struct avr_flash flash_data;
 	char boot_path[1024] = "secure_bootloader.hex"; // Default hex
-    uint32_t boot_base = BLS_START;
+    uint32_t boot_base;
     uint32_t boot_size;
 	char * mmcu = MCU;
 	uint32_t freq = CLOCK;
