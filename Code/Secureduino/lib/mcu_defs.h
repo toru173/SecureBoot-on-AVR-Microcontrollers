10fc3ef7d89e77304d457958e7ddb709358a0e85
toru173 <toru173+github@gmail.com>
2021-04-09 20:37:34 +1000

    Update mcu_defs.h

--- Code/Secureduino/lib/mcu_defs.h
+++ Code/Secureduino/lib/mcu_defs.h
@@ -3,9 +3,9 @@
  */
 
 // valid MCUs include atmega328p or atmega2560
-
 #define MCU "atmega2560"
+
 #define CLOCK 16000000
 // Define bootloader address. 4K on atmega328p = 0x7000, 8K on atmega2560 = 0x3FE00
-#define BLS_START 0x7000
+#define BLS_START 0x3FE00
 #define UART_BAUD 9600
