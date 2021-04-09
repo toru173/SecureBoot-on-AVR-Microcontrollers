6acdd1c051c1fbf5d59916de829ca568efc704c1
toru173 <toru173+github@gmail.com>
2021-04-09 20:26:10 +1000

    Update mcu_defs.h

--- Code/Secureduino/lib/mcu_defs.h
+++ Code/Secureduino/lib/mcu_defs.h
@@ -3,16 +3,16 @@
  */
 
 // valid MCUs include atmega328p or atmega2560
-#define ATMEGA328p "atmega328p"
+#define ATMEGA328P "atmega328p"
 #define ATMEGA2560 "atmega2560"
 
 #define MCU ATMEGA2560
 #define CLOCK 16000000
 
 // Define bootloader address. 4K on atmega328p = 0x7000, 8K on atmega2560 = 0x3FE00
-#if MCU == "atmega328p"
+#if MCU == ATMEGA328P
     #define BLS_START 0x7000
-#elif MCU == "atmega2560"
+#elif MCU == ATMEGA2560
     #define BLS_START 0x3FE00
 #else
     #error "Unexpected MCU"
