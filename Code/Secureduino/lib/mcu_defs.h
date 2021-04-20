60c22c333bbc31a23e25fa776cc7f121e33bd818
toru173 <toru173+github@gmail.com>
2021-04-21 00:00:59 +1000

    Update mcu_defs.h

--- Code/Secureduino/lib/mcu_defs.h
+++ Code/Secureduino/lib/mcu_defs.h
@@ -7,7 +7,7 @@
 // Define bootloader address. 4K space on atmega328p = 0x7000, 8K on atmega2560 = 0x3FE00
 #define BLS_START 0x7000
 
-#define ROM_TOP BLS_START - 1024
+#define ROM_TOP (BLS_START - 0x80) // Need 1024 bits for the signature
 
 #define CLOCK 20000000
 #define BAUD 9600
