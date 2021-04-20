2ae133897d00f2e3d98516d934e47457f80d62d1
toru173 <toru173+github@gmail.com>
2021-04-20 11:12:45 +1000

    Change to 328p

--- Code/Secureduino/secure_payload.c
+++ Code/Secureduino/secure_payload.c
@@ -26,7 +26,7 @@
  * information for the simulator
  */
 #include "avr_mcu_section.h"
-AVR_MCU(F_CPU, "atmega2560");
+AVR_MCU(F_CPU, "atmega328p");
 
 FILE uart_stdio = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
